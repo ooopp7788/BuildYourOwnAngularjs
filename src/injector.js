44026ebe9b4edec17c65a633767bc8298499b295
liumengjie <liumengjie@acfun.tv>
2017-04-28 01:10:46 +0800

    循环依赖

--- src/injector.js
+++ src/injector.js
@@ -1,12 +1,24 @@
 function createInjector(modulesToLoad) {
+    // provderCache是provider的缓存，并没有实例化，只是储存这些provider（function）
+    // 1、provider仅仅是把所有module.provider('xxx',{$get:function(){...}})给储存起来
+    // 2、在loader中push到invokeQueue中
+    // 3、在injector中遍历invokeQueue，将provider储存到providerCache中，并没有实际将依赖注入
+    
+    // instanceCache是实例缓存，依赖的实例
+    // 1、injector.get('xxx')，使用暴露出来的getService方法
+    // 2、先会从instanceCache中查找xxx实例，再从providerCache中查找是否有这个方法
+    // 3、有的话，使用invoke调用它，提取区中的参数（也就是依赖），用getService继续找实例和provider
+    // 4、
+    
     var providerCache = {}; // 用于储存provider
     var instanceCache = {}; // 用于储存constant
     var loadedModules = {};
     var FN_ARGS = /^function\s*[^\(]*\(\s*([^\)]*)\)/m; // 判断是否是函数的正则
     var FN_ARG = /^\s*(_?)(\S+?)\1\s*$/; // 去除空格 两边下划线
     var STRIP_COMMENTS = /(\/\/.*$)|(\/\*.*?\*\/)/mg; // 去除注释
-    var INSTANTIATING = {}
-        // $provid与_invokeQueue相对应
+    var INSTANTIATING = {};
+    
+    // $provid与_invokeQueue相对应
     var $provide = {
         constant: function(key, value) {
             if (key === 'hasOwnProperty') {
@@ -24,12 +36,21 @@ function createInjector(modulesToLoad) {
 
     // 区分2种cache
     function getService(name) {
+        // 先判断有没有实例cache
         if (instanceCache.hasOwnProperty(name)) {
             if (instanceCache[name] === INSTANTIATING) {
                 throw new Error('Circular dependency found');
             }
             return instanceCache[name];
+            //再判断有没有providerCache
         } else if (providerCache.hasOwnProperty(name + 'Provider')) {
+            // 第一次进入时provider都没有实例化
+            // aProvider、bProvider、cProvider ：getService('a');instanceCache[a]=INSTANTIATING  —-依赖—>   b  -->  invoke(bProvider.$get)
+            //  --> getService('b');instanceCache[b]=INSTANTIATING
+            // 同理得getService('c');instanceCache[c]=INSTANTIATING
+            // 死循环的时候，第二次循环会判断instanceCache存在，进入上面循环
+            // 如果存在的实例等于这个假实例的时候，判断为死循环，throw Error
+            instanceCache[name] = INSTANTIATING;
             var provider = providerCache[name + 'Provider'];
             var instance = instanceCache[name] = invoke(provider.$get);
             return instance;
